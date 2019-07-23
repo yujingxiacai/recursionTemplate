1409c9548d83451166e9c0ed82647f5ec6af2785
unknown <shenyukang@360buyad.local>
2019-07-23 20:00:50 +0800

    saf

--- vuetest/src/components/Tree.vue
+++ vuetest/src/components/Tree.vue
@@ -47,17 +47,12 @@ export default {
     methods:{
         
         chooseFather(index){
-            console.log(index);
             // this.showCurrent[index] = !this.showCurrent[index];
             // 这样用是不合理的，用到了强制更新视图，我们上面改变的数据并没有及时更新，原因是因为vue不支持这样的修改
             // this.$forceUpdate()
             // 遇到这样的数据类型需要动态更新监听的话，只能是使用下面的方法，才能监听到。
             //  原因参照原文：https://cn.vuejs.org/v2/guide/list.html#%E6%B3%A8%E6%84%8F%E4%BA%8B%E9%A1%B9
-            this.$set(this.showCurrent, index, !this.showCurrent[index]);
-            console.log(this.showCurrent);
-
-            
-            
+            this.$set(this.showCurrent, index, !this.showCurrent[index]);         
         }
     },
 }
